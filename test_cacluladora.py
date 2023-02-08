cd6e898439279c1d07406cc106db64c02e3df43b
Naylea Castrellón <35163676@uaz.edu.mx>
2023-02-08 14:01:38 -0600

    prueba 10mas2

--- test_cacluladora.py
+++ test_cacluladora.py
@@ -4,7 +4,7 @@ from calculadora import Calculadora
 class TestCalculadora(unittest.TestCase):
 	def test_2_mas_2(self):
 		calc = Calculadora()
-		self.assertEqual(4,calc.sumar(2,2))
+		self.assertEqual(11,calc.sumar(10,2))
 
 if __name__ == '__main__':
 	unittest.main()
